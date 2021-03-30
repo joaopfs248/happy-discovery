76f865496ccd639f4bd45e3a83097d63e9ca8bb2
João Santos <joaopfs@outlook.com.br>
2021-03-29 21:32:03 -0300

    modified database folder

--- src/database/test.js
+++ src/database/test.js
@@ -2,57 +2,40 @@ const Database = require('./db');
 const saveOrphanage = require('./saveOrphanage');
 
 Database.then(async db => {
-    
-    // const orphanage = {
-    //     lat: "-22.9356077",
-    //     lng: "-43.1805307", 
-    //     name: "Fundação Romão Mattos Duarte",
-    //     about: "Presta assistência a crianças de 06 à 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
-    //     whatsapp: "+552125587961",
-    //     images: [ 
-    //         "https://lh4.googleusercontent.com/-wst4vjm-ZNk/V_z1MNBNPXI/AAAAAAAAaf0/N4_9ZXyIKdQWQJiHZwwbL2nepEhp-4fhQCLIB/photo.jpg",
-
-    //         "https://s2.glbimg.com/BnWntNMTzHkUM0M8b3o3aRBDwbA=/i.glbimg.com/og/ig/infoglobo1/f/original/2018/06/27/77583610_rio_de_janeiro_rj_27-06-1988_-_policia_rj_-_maus_tratos_-_menor_-_jose_augusto_a_esquerda.jpg"
-    //     ].toString(),
-    //     instructions: "Venha, sinta-se a vontade e traga muito amor e paciência para dar.",
-    //     opening_hours: "Horário de visitas Das 8h até 18h",
-    //     open_on_weekends: "0"
-    // }
-    // const orphanage = {
-    //     lat: "-22.9008923",
-    //     lng: "-43.3552445", 
-    //     name: "Anbec - Orf. Santa Rita De Cassia",
-    //     about: "Presta assistência para as meninas e adolescentes em situação de vulnerabilidade social em dois programas: Acolhimento institucional na faixa etária de 04 a 10 anos (abrigo) e Casa Dia faixa etária de 04 a 12 anos (externato).",
-    //     whatsapp: "+552124252207",
-    //     images: [ 
-    //         "https://orfanato.portalcyber.com.br/wp-content/uploads/2020/08/WhatsApp-Image-2020-08-12-at-20.59.46-1-1024x512.jpeg",
-
-    //         "https://orfanatosantaritadecassia.com.br/wp-content/uploads/2020/08/capa-foto-2.jpg",
-
-    //         "https://orfanatosantaritadecassia.com.br/wp-content/uploads/2020/08/QR-Code-Orfanato-229x300.png"
-    //     ].toString(),
-    //     instructions: "Visite-nos, sinta-se a vontade e traga muito amor e paciência para dar.",
-    //     opening_hours: "Horário de visitas Das 8h até 18h",
-    //     open_on_weekends: "0"
-    // }
     const orphanage = {
-        lat: "-22.9191906",
-        lng: "-43.2372055", 
-        name: "ABCDEF - Orf. Fictício",
-        about: "Presta assistência para todos os gêneros e idade em situação de vulnerabilidade social.",
-        whatsapp: "+552122222222",
+        lat: "-22.9008923",
+        lng: "-43.3552445", 
+        name: "Anbec - Orf. Santa Rita De Cassia",
+        about: "Presta assistência para as meninas e adolescentes em situação de vulnerabilidade social em dois programas: Acolhimento institucional na faixa etária de 04 a 10 anos (abrigo) e Casa Dia faixa etária de 04 a 12 anos (externato).",
+        whatsapp: "+5521988490203",
         images: [ 
-            "https://static.vakinha.com.br/uploads/vakinha/image/209242/Orfanato-Aulas2.jpg?ims=700x410",
+            "https://orfanato.portalcyber.com.br/wp-content/uploads/2020/08/WhatsApp-Image-2020-08-12-at-20.59.46-1-1024x512.jpeg",
 
-            "https://www.kickante.com.br/sites/default/files/styles/campaign_pitch_image/public/financiamento-coletivo/pitch/natal-solidario-orfanato-502967.jpg?itok=HgpNI35m",
-
-            "https://compartilheviagens.com.br/wp-content/uploads/2016/12/destaque.png"
+            "https://orfanatosantaritadecassia.com.br/wp-content/uploads/2020/08/capa-foto-2.jpg"
         ].toString(),
         instructions: "Visite-nos, sinta-se a vontade e traga muito amor e paciência para dar.",
         opening_hours: "Horário de visitas Das 8h até 18h",
-        open_on_weekends: "1"
+        open_on_weekends: "0"
     }
+    // const orphanage = {
+    //     lat: "-22.9084977",
+    //     lng: "-43.2471823", 
+    //     name: "Lar de Maria Dolores",
+    //     about: "Temos como objetivo social promover a assistência social, às pessoas, de quaisquer procedência, em especial à criança e ao adolescente, em situação de risco ou vulnerabilidade social e pessoal, sem discriminação de etnia, gênero, orientação sexual e religiosa, bem como a portadores de deficiência ou necessidades especiais, por meio de oferta gratuita de serviços socioassistenciais, programas e projetos de proteção social básica e especial, assegurando a função protetiva à família e fortalecendo os vínculos familiares, sociais e comunitários.",
+    //     whatsapp: "+5521970311837",
+    //     images: [ 
+    //         "https://www.lardemariadolores.org.br/wp-content/uploads/2018/05/site5.jpg",
 
+    //         "https://www.lardemariadolores.org.br/wp-content/uploads/2018/05/violino-site1-1.jpg",
+
+    //         "https://www.lardemariadolores.org.br/wp-content/uploads/2018/05/gestante-site3.jpg",
+
+    //         "https://www.lardemariadolores.org.br/wp-content/uploads/2018/05/oficina-dos-pais.jpg"
+    //     ].toString(),
+    //     instructions: "Conheça e faça uma doação diretamente para as causas que seu coração mais se identificar.",
+    //     opening_hours: "Horário de visitas Das 6h até 18h",
+    //     open_on_weekends: "1"
+    // }
 
     //inserir dados na tabela
     await saveOrphanage(db, orphanage)
@@ -67,5 +50,4 @@ Database.then(async db => {
 
     // apagar dados da tabela por id
     // console.log(await db.run('DELETE FROM orphanages WHERE id = "1" '))
-     console.log(await db.run('DELETE FROM orphanages WHERE id = "3" '))
 })
